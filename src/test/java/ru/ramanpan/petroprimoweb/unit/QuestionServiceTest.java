a751b23b71f71a1813b521db9d937150057e16ae
RamanPan <ramanpan123@gmail.com>
2022-09-03 16:52:37 +0300

    Start doing tests for QuestionService

--- src/test/java/ru/ramanpan/petroprimoweb/unit/QuestionServiceTest.java
+++ src/test/java/ru/ramanpan/petroprimoweb/unit/QuestionServiceTest.java
@@ -3,13 +3,24 @@ package ru.ramanpan.petroprimoweb.unit;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.ramanpan.petroprimoweb.repository.QuestionRepo;
+import ru.ramanpan.petroprimoweb.service.AnswerService;
 import ru.ramanpan.petroprimoweb.service.QuestionService;
+import ru.ramanpan.petroprimoweb.service.TestService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QuestionServiceTest {
     @Autowired
     public QuestionService questionService;
+    @MockBean
+    private QuestionRepo questionRepo;
+    @MockBean
+    private TestService testService;
+    @MockBean
+    private AnswerService answerService;
+
 
 }
